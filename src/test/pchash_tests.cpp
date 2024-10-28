af199dfe3300ebd34c813e720722f25aee7a842a
moonsettler <moonsettler@protonmail.com>
2024-10-28 01:35:13 +0100

    Fix PairCommitHash replace std::vector serilaization to direct serilaization

--- src/test/pchash_tests.cpp
+++ src/test/pchash_tests.cpp
@@ -19,22 +19,9 @@
 #include <univalue.h>
 
 namespace {
-    struct TestVector {
-        std::vector<unsigned char> x1;
-        std::vector<unsigned char> x2;
-        public: uint256 hash;
-        uint256 check_result;
+    typedef std::vector<unsigned char> valtype;
 
-        TestVector(std::string s1, std::string s2)
-        {
-            x1 = std::vector<unsigned char>(s1.begin(), s1.end());
-            x2 = std::vector<unsigned char>(s2.begin(), s2.end());
-
-            hash = PairCommitHash(x1, x2);
-        }
-    };
-
-    // SHA256("PariCommit")
+    // SHA256("PairCommit")
     static const unsigned char pc_tag_hash[32] = {
         0x08, 0x20, 0x7e, 0x7c, 0x41, 0xc4, 0x78, 0x33,
         0xaa, 0x39, 0x74, 0x77, 0xdf, 0x01, 0xc9, 0xde,
@@ -43,27 +30,18 @@ namespace {
     };
 
     static const unsigned char test1_expected_result[32] = {
-        0x9b, 0xb3, 0x05, 0xa2, 0x0c, 0xc1, 0xc7, 0x30, 
-        0xd2, 0xbc, 0x71, 0x3b, 0x6f, 0x79, 0x21, 0x1a, 
-        0xc8, 0x7f, 0x5d, 0xdb, 0xfd, 0xa7, 0x44, 0xff, 
-        0xfc, 0xdb, 0x96, 0xb9, 0xac, 0xb8, 0xc5, 0xfc
+        0x57, 0xaf, 0x85, 0x35, 0xe7, 0x7b, 0xee, 0x32,
+        0xc3, 0x16, 0x84, 0xec, 0x4b, 0x58, 0x61, 0x55,
+        0xd2, 0x6a, 0xff, 0x07, 0x7d, 0xc6, 0xf7, 0xd7,
+        0x95, 0x4a, 0x51, 0xc8, 0x62, 0x0a, 0xff, 0xcc
     };
-
-    TestVector test1 = TestVector("Hello ", "World!");
 
     static const unsigned char test2_expected_result[32] = {
-        0x4d, 0x82, 0x4b, 0x3c, 0x93, 0xd5, 0x2e, 0x26,
-        0x32, 0x67, 0x99, 0x06, 0xe7, 0x89, 0xf1, 0xcc,
-        0xb0, 0x39, 0xa9, 0xe8, 0x33, 0x91, 0x90, 0xc2,
-        0x66, 0xd4, 0xe7, 0xe1, 0x33, 0x25, 0x9f, 0x50       
+        0xd3, 0xed, 0x6d, 0x42, 0x4f, 0x70, 0xb3, 0x19,
+        0x4c, 0x20, 0x0c, 0x0c, 0xb1, 0x9c, 0xaf, 0xbb,
+        0x2b, 0xc1, 0x2e, 0xcf, 0x8d, 0x4c, 0x00, 0x0b,
+        0x9e, 0x1c, 0x1e, 0x28, 0x3b, 0xdc, 0xb8, 0x59    
     };
-
-    TestVector test2 = TestVector("Hello", " World!");
-
-    void RunTest(const TestVector& test, uint256 expected_result)
-    {
-        BOOST_CHECK_EQUAL(test.hash, uint256(expected_result));
-    }
 }
 
 BOOST_FIXTURE_TEST_SUITE(pchash_tests, BasicTestingSetup)
@@ -71,34 +49,40 @@ BOOST_FIXTURE_TEST_SUITE(pchash_tests, BasicTestingSetup)
 // Goal: check that PC Hash Function generate correct hash
 BOOST_AUTO_TEST_CASE(pchash_from_data)
 {
-    RunTest(test1, uint256(test1_expected_result));
+    uint256 hash1 = PairCommitHash(
+        valtype{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20},
+        valtype{0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21}
+    );
+    BOOST_CHECK_EQUAL(hash1, uint256(test1_expected_result));
 
-    RunTest(test2, uint256(test2_expected_result));
+
+    uint256 hash2 = PairCommitHash(
+        valtype{0x48, 0x65, 0x6c, 0x6c, 0x6f},
+        valtype{0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21}
+    );
+    BOOST_CHECK_EQUAL(hash2, uint256(test2_expected_result));
 }
 
 // Goal: check that PC Hash Function generate correct hash
-/*
 BOOST_AUTO_TEST_CASE(pchash_reproduce)
 {
-    std::string s1 = "Hello ";
-    std::string s2 = "World!";
-    std::vector<unsigned char> x1(s1.begin(), s1.end());
-    std::vector<unsigned char> x2(s2.begin(), s2.end());
+    const valtype x1 = {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20};
+    const valtype x2 = {0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21};
 
     HashWriter ss;
     ss << pc_tag_hash
        << pc_tag_hash
-       << x1
-       << x2
-       << uint32_t(6)
+       << Span<const unsigned char>{x1.data(), x1.size()}
+       << Span<const unsigned char>{x2.data(), x2.size()}
+       << uint32_t(x1.size())
        << uint32_t(0x01000000)
-       << uint32_t(6)
+       << uint32_t(x2.size())
        << uint32_t(0x01000000);
 
     uint256 check1 = ss.GetSHA256();
+    uint256 check2 = PairCommitHash(x1, x2);
 
-    BOOST_CHECK_EQUAL(test1.hash, check1);
+    BOOST_CHECK_EQUAL(check1, check2);
 }
-*/
 
 BOOST_AUTO_TEST_SUITE_END()
