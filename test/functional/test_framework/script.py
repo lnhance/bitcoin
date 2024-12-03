b3469e88bc3ebcb8dd45423ac15e30bb5fca28e2
moonsettler <moonsettler@protonmail.com>
2024-12-03 21:45:59 +0100

    Add OP_PAIRCOMMIT to LNHANCE

--- test/functional/test_framework/script.py
+++ test/functional/test_framework/script.py
@@ -254,6 +254,7 @@ OP_NOP10 = CScriptOp(0xb9)
 OP_CHECKSIGADD = CScriptOp(0xba)
 OP_INTERNALKEY = CScriptOp(0xcb)
 OP_CHECKSIGFROMSTACK = CScriptOp(0xcc)
+OP_PAIRCOMMIT = CScriptOp(0xcd)
 
 OP_INVALIDOPCODE = CScriptOp(0xff)
 
@@ -372,6 +373,7 @@ OPCODE_NAMES.update({
     OP_CHECKSIGADD: 'OP_CHECKSIGADD',
     OP_INTERNALKEY: 'OP_INTERNALKEY',
     OP_CHECKSIGFROMSTACK: 'OP_CHECKSIGFROMSTACK',
+    OP_PAIRCOMMIT: 'OP_PAIRCOMMIT',
     OP_INVALIDOPCODE: 'OP_INVALIDOPCODE',
 })
 
@@ -929,4 +931,4 @@ def taproot_construct(pubkey, scripts=None, treat_internal_as_infinity=False):
     return TaprootInfo(CScript([OP_1, tweaked]), pubkey, negated + 0, tweak, leaves, h, tweaked)
 
 def is_op_success(o):
-    return o == 0x50 or o == 0x62 or o == 0x89 or o == 0x8a or o == 0x8d or o == 0x8e or (o >= 0x7e and o <= 0x81) or (o >= 0x83 and o <= 0x86) or (o >= 0x95 and o <= 0x99) or (o >= 0xbb and o <= 0xca) or (o >= 0xcd and o <= 0xfe)
+    return o == 0x50 or o == 0x62 or o == 0x89 or o == 0x8a or o == 0x8d or o == 0x8e or (o >= 0x7e and o <= 0x81) or (o >= 0x83 and o <= 0x86) or (o >= 0x95 and o <= 0x99) or (o >= 0xbb and o <= 0xca) or (o >= 0xce and o <= 0xfe)
